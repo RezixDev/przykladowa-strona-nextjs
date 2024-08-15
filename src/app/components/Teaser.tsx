0c43a791e40edb2431a0631c4a225f7cf7ca3233
RezixDev <mateuszrysownik2@gmail.com>
2024-08-15 22:51:42 +0200

    test

--- src/app/components/Teaser.tsx
+++ src/app/components/Teaser.tsx
@@ -4,7 +4,7 @@ export const Teaser = () => {
     <div className="flex flex-col md:flex-row items-center justify-between max-w-7xl mx-auto py-12 px-4">
       <div className="md:w-1/2 mb-8 md:mb-0 relative w-full" style={{ height: '500px' }}>
         <img 
-          src="/doggo.png" 
+          src="doggo.png" 
           alt="Teaser image" 
           style={{ objectFit: 'cover' }}
           className="rounded-lg"
